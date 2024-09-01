431cd7ad0b8ca7b03a72fc168ef3eef2fb6c7788
Kyle Mardell <kyleslufc@gmail.com>
2024-09-01 22:23:21 +0000

    Add initial resource detail page.

--- post/urls.py
+++ post/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+    path('<slug:slug>/', views.resource_detail, name='resource_detail'),
     path('', views.ResourceList.as_view(), name='home'),
 ]
--- post/views.py
+++ post/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Resource
 
@@ -8,3 +8,27 @@ class ResourceList(generic.ListView):
     queryset = Resource.objects.filter(status=1)
     template_name = "post/index.html"
     paginate_by = 6
+
+
+def resource_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    queryset = Resource.objects.filter(status=1)
+    resource = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "post/resource_detail.html",
+        {"resource": resource},
+    )
