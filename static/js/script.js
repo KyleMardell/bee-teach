73cf511c38a2111e195889ce83fbb791398a9d1f
Kyle Mardell <kyleslufc@gmail.com>
2024-09-30 22:22:42 +0000

    Refactor the script.js code to display key stages correctly for both draft and published posts

--- static/js/script.js
+++ static/js/script.js
@@ -9,23 +9,27 @@ document.addEventListener('DOMContentLoaded', function () {
         commentFormLabel.style.display = "none";
     }
 
-    const keyStageLabel = document.getElementById('key-stage'); // Store the element reference
+    let keyStageLabels = document.getElementsByClassName('key-stage'); // Store the element reference
+    console.log(keyStageLabels);
 
-    switch (keyStageLabel.innerText) {  // Use innerText to get the element's text
-        case "0":
-            keyStageLabel.innerText = "Key-Stage: Early years";
-            break;
-        case "1":
-            keyStageLabel.innerText = "Key-Stage: 1";
-            break;
-        case "2":
-            keyStageLabel.innerText = "Key-Stage: 2";
-            break;
-        case "3":
-            keyStageLabel.innerText = "Key-Stage: 3";
-            break;
-        default:
-            keyStageLabel.innerText = "Key-Stage: Not Found";
-            break;
+    for (let keyStageLabel of keyStageLabels) {
+
+        switch (keyStageLabel.innerText) {  // Use innerText to get the element's text
+            case "0":
+                keyStageLabel.innerText = "Key-Stage: Early years";
+                break;
+            case "1":
+                keyStageLabel.innerText = "Key-Stage: 1";
+                break;
+            case "2":
+                keyStageLabel.innerText = "Key-Stage: 2";
+                break;
+            case "3":
+                keyStageLabel.innerText = "Key-Stage: 3";
+                break;
+            default:
+                keyStageLabel.innerText = "Key-Stage: Not Found";
+                break;
+        }
     }
 });
