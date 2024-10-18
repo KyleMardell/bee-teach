1793889de9afa9969a4fa0f4ba91694b4775ec5d
Kyle Mardell <kyleslufc@gmail.com>
2024-10-18 22:31:42 +0000

    Add index and home page tests

--- post/test_views.py
+++ post/test_views.py
@@ -7,11 +7,18 @@ from .models import Resource, Media
 class TestPostViews(TestCase):
 
     def setUp(self):
+
         self.user = User.objects.create_superuser(
             username="myUsername",
             password="myPassword",
             email="test@test.com"
         )
+
+        self.client.login(
+            username="myUsername",
+            password="myPassword"
+        )
+
         self.resource = Resource.objects.create(
                         title="Resource Title",
                         author=self.user,
@@ -20,6 +27,15 @@ class TestPostViews(TestCase):
                         content="Resource content",
                         links="www.testlinks.com",
                         status=1)
+
+        self.draft_resource = Resource.objects.create(
+                        title="Draft Resource Title",
+                        author=self.user,
+                        slug="draft-resource-title",
+                        key_stage="0",
+                        content="Draft resource content",
+                        links="www.testlinks.com",
+                        status=0)
 
         for i in range(4):  # Create additional resources to test the home page view.
             Resource.objects.create(
@@ -32,27 +48,70 @@ class TestPostViews(TestCase):
                 status=1
             )
 
-    # Home Page
+    # Index Page
 
-    def test_home_page_status_code(self):
+    def test_index_page_status_code(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
 
-    def test_home_page_uses_correct_template(self):
+    def test_index_page_uses_correct_template(self):
         response = self.client.get(reverse('home'))
         self.assertTemplateUsed(response, 'post/index.html')
 
-    def test_home_page_shows_four_latest_resources(self):
+    def test_index_page_shows_four_latest_resources(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context['resources']), 4)
-        self.assertContains(response, "Resource Title 3")
         self.assertContains(response, "Resource Title 0")
+        self.assertContains(response, "Resource Title 3")
+
+    def test_index_page_shows_only_published_resources(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Draft Resource Title")
+
+    # Home Page (resource_list)
+
+    def test_home_page_status_code(self):
+        response = self.client.get(reverse('resource_list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_requires_authentication(self):
+        self.client.logout()
+        response = self.client.get(reverse('resource_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/accounts/login/?next=/resource_list/')
+
+    def test_home_page_uses_correct_template(self):
+        response = self.client.get(reverse('resource_list'))
+        self.assertTemplateUsed(response, 'post/resource_list.html')
+
+    def test_partial_template(self):
+        response = self.client.get(reverse('resource_list') + '?page=1')
+        self.assertTemplateUsed(response, 'post/partials/resource_list.html')
+
+    def test_home_page_shows_only_published_resources(self):
+        response = self.client.get(reverse('resource_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, "Draft Resource Title")
+
+    def test_featured_resources(self):
+        response = self.client.get(reverse('resource_list'))
+        self.assertIn('features', response.context)
 
     # Resource Detail Page
 
+    def test_resource_detail_page_status_code(self):
+        response = self.client.get(reverse(
+            'resource_detail', args=['resource-title']))
+        self.assertEqual(response.status_code, 200)
+
+    def test_resource_detail_uses_correct_template(self):
+        response = self.client.get(reverse(
+            'resource_detail', args=['resource-title']))
+        self.assertTemplateUsed(response, 'post/resource_detail.html')
+
     def test_render_resource_detail_page_with_comment_form(self):
-        self.client.login(username="myUsername", password="myPassword")
         response = self.client.get(reverse(
             'resource_detail', args=['resource-title']))
         self.assertEqual(response.status_code, 200)
@@ -62,14 +121,9 @@ class TestPostViews(TestCase):
             response.context['comment_form'], CommentForm)
 
     def test_successful_comment_submission(self):
-        self.client.login(username="myUsername", password="myPassword")
         post_data = {'body': 'This is a test comment.'}
         response = self.client.post(reverse(
             'resource_detail', args=['resource-title']), post_data)
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'Comment Added', response.content)
         self.assertIn(b'This is a test comment.', response.content)
-
-    
-
-    
