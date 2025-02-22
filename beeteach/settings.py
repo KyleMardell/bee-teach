b12f9354358e322afa02bdceb575f4900beb1d9f
Kyle Mardell <151871777+KyleMardell@users.noreply.github.com>
2025-02-22 18:37:34 +0000

    Change debug to false.

--- beeteach/settings.py
+++ beeteach/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-kylemardell-beeteach-kd1gl8x9vwg.ws.codeinstitute-ide.net',
